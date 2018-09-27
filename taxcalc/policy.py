7458d81ea4d6510956021680c0df4e200d24f074
andersonfrailey <anderson.frailey@aei.org>
2018-09-27 16:27:22 +0530

    add additional years

--- taxcalc/policy.py
+++ taxcalc/policy.py
@@ -44,7 +44,7 @@ class Policy(ParametersBase):
     DEFAULTS_FILENAME = 'current_law_policy.json'
     JSON_START_YEAR = 2017  # remains the same unless earlier data added
     LAST_KNOWN_YEAR = 2017  # last year for which indexed param vals are known
-    LAST_BUDGET_YEAR = 2017  # increases by one for every new assessment year
+    LAST_BUDGET_YEAR = 2019  # increases by one for every new assessment year
     DEFAULT_NUM_YEARS = LAST_BUDGET_YEAR - JSON_START_YEAR + 1
 
     def __init__(self,
