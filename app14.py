ab9734363f7b2e07bf32a2fbf8341a3e373b3753
shekharkhetan <shekharkhetan@gmail.com>
2018-11-16 16:54:12 +0530

    changes in test_parameters.py file

--- app14.py
+++ app14.py
@@ -22,10 +22,11 @@ def gti_format(gti):
     final_str = f'Rs. {pt1:16} - {pt2:15}'
     return final_str
 
+
 locale.setlocale(locale.LC_ALL, '')
 
 # create Records object containing pit.csv and pit_weights.csv input data
-recs = Records(data='pitBigData.csv',weights= 'pit_weightsBD.csv')
+recs = Records(data='pitBigData.csv', weights='pit_weightsBD.csv')
 
 # create Policy object containing current-law policy
 pol = Policy()
@@ -58,7 +59,8 @@ dumpdf['pitax1'] = calc1.array('pitax')
 dumpdf['pitax2'] = calc2.array('pitax')
 dumpdf['pitax_diff'] = dumpdf['pitax2'] - dumpdf['pitax1']
 dumpdf['percent change in tax'] = np.where(dumpdf['pitax_diff'] == 0.0,
-                                           0.0, (dumpdf['pitax_diff']/dumpdf['pitax1'] * 100))
+                                           0.0, (dumpdf['pitax_diff'] /
+                                                 dumpdf['pitax1'] * 100))
 dumpdf['Zero change'] = np.where(dumpdf['percent change in tax'] == 0.0, 1, 0)
 column_order = dumpdf.columns
 
@@ -67,8 +69,9 @@ assert len(dumpdf.index) == calc1.array_len
 dumpdf.to_csv('app14-dump.csv', columns=column_order,
               index=False, float_format='%.0f')
 pd.options.display.float_format = 'Rs.{:,.0f}'.format
-# converting the result into deciles and getting mean of three variables each year
-df1 = dumpdf.groupby(pd.qcut(dumpdf.GTI, 10))['pitax1', 'pitax2', 'pitax_diff'].mean()
+# converting the result into deciles and getting mean of 3 variables each yr
+df1 = dumpdf.groupby(pd.qcut(dumpdf.GTI, 10))['pitax1', 'pitax2',
+                                              'pitax_diff'].mean()
 # making it more beautiful
 renames = {'pitax1': 'Base', 'pitax2': 'Reform', 'pitax_diff': 'Difference'}
 df1 = df1.reset_index()
@@ -77,9 +80,10 @@ df1.rename(renames, axis=1, inplace=True)
 df1.index = [i for i in range(1, 11)]
 print('\nIndividual Level - Average by Decile 2019')
 print(df1)
-df1.to_csv('Decilemeans.csv') #conversion to csv files
+df1.to_csv('Decilemeans.csv')  # conversion to csv files
 
-df2 = dumpdf.groupby(pd.qcut(dumpdf.GTI, 10))['pitax1', 'pitax2', 'pitax_diff'].sum()
+df2 = dumpdf.groupby(pd.qcut(dumpdf.GTI, 10))['pitax1', 'pitax2',
+                                              'pitax_diff'].sum()
 df2 = df2.reset_index()
 df2['GTI'] = df2['GTI'].apply(gti_format)
 df2.rename(renames, axis=1, inplace=True)
