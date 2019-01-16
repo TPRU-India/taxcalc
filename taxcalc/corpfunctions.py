a15285f60fc53ff669ec50c791baecc84eddd074
kumar-ab <iitkgp.abhinav@gmail.com>
2019-01-16 18:31:47 +0530

    Net loss and carryforward logic

--- taxcalc/corpfunctions.py
+++ taxcalc/corpfunctions.py
@@ -23,17 +23,39 @@ def net_tax_liability_a(NET_TAX_LIABILTY, NET_TAX_LIABILITY_B,
 
 
 @iterate_jit(nopython=True)
-def corp_GTI(INCOME_HP, Income_BP, ST_CG_AMT_1, ST_CG_AMT_2, ST_CG_AMT_APPRATE,
-             LT_CG_AMT_1, LT_CG_AMT_2, TOTAL_INCOME_OS, CY_Losses, BF_Losses,
-             GTI):
+def corp_GTI_before_set_off(INCOME_HP, Income_BP, ST_CG_AMT_1, ST_CG_AMT_2,
+                            ST_CG_AMT_APPRATE, LT_CG_AMT_1, LT_CG_AMT_2,
+                            TOTAL_INCOME_OS, GTI_Before_Loss):
     """
     Compute GTI including capital gains amounts taxed at special rates.
     """
-    GTI = (INCOME_HP + Income_BP + ST_CG_AMT_1 + ST_CG_AMT_2 +
-           ST_CG_AMT_APPRATE + LT_CG_AMT_1 + LT_CG_AMT_2 +
-           TOTAL_INCOME_OS) - (CY_Losses + BF_Losses)
-    GTI = np.maximum(0., GTI)
-    return GTI
+    GTI_Before_Loss = (INCOME_HP + Income_BP + ST_CG_AMT_1 + ST_CG_AMT_2 +
+                       ST_CG_AMT_APPRATE + LT_CG_AMT_1 + LT_CG_AMT_2 +
+                       TOTAL_INCOME_OS)
+    return GTI_Before_Loss
+
+
+@iterate_jit(nopython=True)
+def GTI_and_losses(Loss_CFLimit, GTI_Before_Loss, CY_Losses,
+                   LOSS_LAG1, LOSS_LAG2, LOSS_LAG3, LOSS_LAG4,
+                   LOSS_LAG5, LOSS_LAG6, LOSS_LAG7, LOSS_LAG8,
+                   GTI, newloss1, newloss2, newloss3, newloss4,
+                   newloss5, newloss6, newloss7, newloss8):
+    LOSS_LAGS = [LOSS_LAG1, LOSS_LAG2, LOSS_LAG3, LOSS_LAG4, LOSS_LAG5,
+                 LOSS_LAG6, LOSS_LAG7, LOSS_LAG8]
+    GTI1 = max(GTI_Before_Loss - CY_Losses, 0.)
+    newloss1 = GTI1 - GTI_Before_Loss + CY_Losses
+    USELOSS = np.zeros(8)
+    for i in range(8, 0, -1):
+        if Loss_CFLimit >= i:
+            USELOSS[i-1] = min(GTI1, LOSS_LAGS[i-1])
+        GTI1 = GTI1 - USELOSS[i-1]
+    NETLOSSES = np.array(LOSS_LAGS) - USELOSS
+    (newloss2, newloss3, newloss4, newloss5, newloss6,
+     newloss7, newloss8) = NETLOSSES[:7]
+    GTI = GTI1
+    return (GTI, newloss1, newloss2, newloss3, newloss4,
+            newloss5, newloss6, newloss7, newloss8)
 
 
 def net_tax_liability_b(calc):
