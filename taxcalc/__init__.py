fd679c33edd6484c40c5d11e77d6cbb00aafe951
Martin Holmer <martin.holmer@gmail.com>
2019-05-31 12:58:31 -0400

    Fix gstfunctions.py to eliminate four test failures

--- taxcalc/__init__.py
+++ taxcalc/__init__.py
@@ -6,8 +6,7 @@ from taxcalc.policy import *
 from taxcalc.records import *
 from taxcalc.calculator import *
 from taxcalc.utils import *
-import pandas as pd
-
-from taxcalc._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from taxcalc.gstrecords import *
+from taxcalc.gstfunctions import *
+from taxcalc.corprecords import *
+from taxcalc.corpfunctions import *
