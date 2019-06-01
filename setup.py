1d7343d895c5c0a292fa8c5e5ea516105b85aec3
Martin Holmer <martin.holmer@gmail.com>
2019-06-01 15:23:28 -0400

    Revisions that allow building/installing a local conda package

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-import versioneer
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'taxcalc/_version.py'
-versioneer.versionfile_build = 'taxcalc/_version.py'
-versioneer.tag_prefix = ''  # tags are like 1.2.0
-versioneer.parentdir_prefix = 'taxcalc-'  # dirname like 'taxcalc-1.2.0'
-
 try:
     from setuptools import setup
 except ImportError:
@@ -13,31 +6,30 @@ except ImportError:
 with open('README.md') as f:
         longdesc = f.read()
 
-version = versioneer.get_version()
-cmdclass = versioneer.get_cmdclass()
+version = '0.0.0'
 
 config = {
-    'description': 'pitaxcalc-demo',
-    'url': 'https://github.com/TPRU-demo/pitaxcalc-demo',
-    'download_url': 'https://github.com/TPRU-demo/pitaxcalc-demo',
-    'description': 'pitaxcalc-demo',
+    'description': 'Tax Microsimulation Model for India',
+    'url': 'https://github.com/TPRU-India/taxcalc',
+    'download_url': 'https://github.com/TPRU-India/taxcalc',
+    'description': 'taxcalc',
     'long_description': longdesc,
     'version': version,
-    'cmdclass': cmdclass,
-    'license': 'MIT',
+    'license': 'xxx',
     'packages': ['taxcalc'],
     'include_package_data': True,
     'name': 'taxcalc',
-    'install_requires': ['numpy', 'pandas', 'numba', 'toolz'],
+    'install_requires': ['numpy', 'pandas', 'numba'],
     'classifiers': [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
+        'License :: xxx :: xxx',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules'],
     'tests_require': ['pytest']
 }
