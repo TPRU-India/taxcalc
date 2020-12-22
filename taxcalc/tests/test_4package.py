e83e2d801593719fe6095e0d59a571bfd217e7a1
kumar-ab <iitkgp.abhinav@gmail.com>
2020-12-22 12:00:35 +0530

    code cleanup

--- taxcalc/tests/test_4package.py
+++ taxcalc/tests/test_4package.py
@@ -20,7 +20,7 @@ def test_for_package_existence():
     the local mark prevents test from running on GitHub.
     """
     out = subprocess.check_output(['conda', 'list', 'taxcalc']).decode('ascii')
-    envless_out = out.replace('taxcalc-india-dev', 'environment')
+    envless_out = out.replace('taxcalc-dev', 'environment')
     if re.search('taxcalc', envless_out) is not None:
         assert 'taxcalc package' == 'installed'
 
